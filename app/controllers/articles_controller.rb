5dd20bc6e68cc023be330af3a8e180878cab8abb
ocakmali <ocakmali@gmail.com>
2018-02-10 19:40:49 +0300

    project added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    #@article.save
+    if @article.save
+      flash[:notice] = "Article saved!"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated"
+      redirect_to articles_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] =" Article deleted"
+    redirect_to articles_path(@article)    
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
